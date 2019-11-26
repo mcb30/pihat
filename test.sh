a1c14eb215f9189e7957bb571f7fe059af3c97fe
Michael Brown <mbrown@fensystems.co.uk>
2019-11-26 13:05:29 +0000

    test: Add test.sh wrapper script
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# Run test suite with coverage checks
+#
+coverage3 erase
+coverage3 run --branch --source pihat setup.py test
+coverage3 report --show-missing
+
+# Run mypy
+#
+mypy pihat test
+
+# Run pycodestyle
+#
+python3 -m pycodestyle pihat test
